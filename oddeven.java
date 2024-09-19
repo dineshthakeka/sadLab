a99baacbdac48a110ea6e9daaf51c1e87822f466
dineshthakeka <dinesh.thakekar@gmail.com>
2024-09-19 11:06:45 +0530

    Add files via upload

---
+++ oddeven.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+class oddeven
+{ public static void main(String[] args)
+  { int n;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter A number");
+    n=sc.nextInt();
+    if(n%2==0)
+    {
+       System.out.println("No is A Even");
+       
+    
+    }      
+   else
+   { 
+      System.out.println("No Is Odd");
+      
+    
+   }
+   
+   
+   
+   
+   
+  }
+
+
+
+
+
+
+
+
+
+
+}
